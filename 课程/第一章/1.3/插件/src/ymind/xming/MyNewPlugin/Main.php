ec2d03cf8313a0b9d96baf1bec96a9f10a97a46c
YxMingY <526817087@qq.com>
2018-04-07 20:37:30 +0800

    1.3完成

---
+++ 课程/第一章/1.3/插件/src/ymind/xming/MyNewPlugin/Main.php
@@ -0,0 +1,15 @@
+<?php
+//调用PluginBase类，方便继承
+use pocketmine\plugin\PluginBase;
+//主类
+class Main extends PluginBase{
+   public function onLoad(){
+      //插件加载时执行
+   }
+   public function onEnable(){
+      //插件启动时执行
+   }
+   public function onDisable(){
+      //插件关闭时执行
+   }
+}
