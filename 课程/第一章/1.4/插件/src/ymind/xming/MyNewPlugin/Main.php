a273bedf145d0c816351d9d9ca3326e1cadffcff
YxMingY <526817087@qq.com>
2018-04-08 21:28:05 +0800

    完成1.4

--- 课程/第一章/1.4/插件/src/ymind/xming/MyNewPlugin/Main.php
+++ 课程/第一章/1.4/插件/src/ymind/xming/MyNewPlugin/Main.php
@@ -7,11 +7,17 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase{
    public function onLoad(){
       //插件加载时执行
+      //用Logger在控制台显示普通消息(白字)
+      $this->getLogger()->info("MyNewPlugin 正在加载!");
    }
    public function onEnable(){
       //插件启动时执行
+      //用Logger在控制台显示公告消息(蓝字)
+      $this->getLogger()->notice("MyNewPlugin 已启动! 作者:xMing");
    }
    public function onDisable(){
       //插件关闭时执行
+      //用Logger在控制台显示警告消息(黄/红字)
+      $this->getLogger()->warning("MyNewPlugin 已关闭!");
    }
 }
